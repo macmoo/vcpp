e1ed61f3da196c557a728a46da9a6bf95ec6a828
macmoo <hscrollhooli@gmail.com>
2023-01-29 12:06:42 +0900

    20230129

--- 202211/p39/p39/p39/p39.cpp
+++ 202211/p39/p39/p39/p39.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 
+using namespace std;
+// ----------------------------------------
+void* operator new(size_t objSize)
+{
+    cout << objSize << " byte allocated." << endl;
+    return (malloc(objSize));
+}
+// ----------------------------------------
+void* operator new[](size_t objSize)
+{
+    cout << objSize << " byte(array) allocated." << endl;
+    return (malloc(objSize));
+}
+// ----------------------------------------
+void operator delete(void* p)
+{
+    cout << hex << p << " memory deleted." << endl;
+}
+// ----------------------------------------
+void operator delete[](void* p)
+{
+    cout << hex << p << " memory(array) deleted." << endl;
+}
+// ----------------------------------------
 int main()
 {
-	std::cout << "hello world!" << std::endl;
+    cout << "----------------------------------------" << endl;
+    int* p1 = (int*)new int;
+    int* p2 = (int*)new int[100];
+
+    delete   p1;
+    delete[] p2;
+    cout << "----------------------------------------" << endl;
 }
